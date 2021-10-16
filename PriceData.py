24db4a3c18438b3c6de872d952c861ab745cf4bb
jefftheref21 <jeffthereff2017@gmail.com>
2021-10-16 13:36:21 -0700

    PriceData
    I accidentally deleted some things and recreated them, so hopefully it is all good.

--- PriceData.py
+++ PriceData.py
@@ -1,18 +1,18 @@
 import yfinance as yf
 import pandas as pd
+import os
 
 open_file = open("Russell1000Tickers.txt", "r")
 requiredTickers = open_file.read().split('\n')
 print(requiredTickers)
 
-df_list = list()
 for ticker in requiredTickers:
     data = yf.download(ticker.strip(), group_by="Ticker", period='max')
+    try:
+        os.mkdir(f'CSV_files/{ticker}')
+    except FileExistsError:
+        print(f'{ticker} directory already exists.')
     data['ticker'] = ticker  # add this column because the dataframe doesn't contain a column with the ticker
-    df_list.append(data)
+    # save to csv
+    data.to_csv('CSV_files/' + ticker + /price.csv')
 
-# combine all dataframes into a single dataframe
-df = pd.concat(df_list)
-
-# save to csv
-df.to_csv('ticker.csv')
