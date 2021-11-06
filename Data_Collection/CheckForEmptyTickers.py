36e531848923ac9683845109a6b8255a536806eb
Kenneth Leeper <grimreaper4002@gmail.com>
2021-11-06 11:57:52 -0800

    Listed empty tickers
    EmptyTickers.txt contains a list of all empty tickers

---
+++ Data_Collection/CheckForEmptyTickers.py
@@ -0,0 +1,21 @@
+# Spacer
+
+import pandas as pd
+
+open_file = open("Russell1000Tickers.txt")
+tickers = open_file.read().splitlines()
+open_file.close()
+types = ("", "Quart")
+emptyTickers = []
+for ticker in tickers:
+    for fileType in types:
+        fileCheck = pd.read_csv(f"CSV_files/{ticker}/{ticker}_{fileType}BalSheet+{fileType}CashFlow.csv")
+        if not fileCheck.values.any():
+            emptyTickers.append(ticker)
+            print(ticker)
+            break
+open_file = open("EmptyTickers.txt", "w")
+for i in emptyTickers:
+    open_file.write(i + "\n")
+open_file.close()
+
