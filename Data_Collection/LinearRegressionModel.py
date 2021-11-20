1b2ce28740e634627bc97728c05eebd87569eaa8
jefftheref21 <jeffthereff2017@gmail.com>
2021-11-19 23:48:26 -0800

    Linear Regression Model but with our own data
    I don't know what variables we should be using. I know Price Return should be the dependent variable but I'm not sure for the independent variable.

--- Data_Collection/LinearRegressionModel.py
+++ Data_Collection/LinearRegressionModel.py
@@ -1,38 +1,69 @@
 import pandas as pd
-from sklearn import linear_model
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model, metrics
 import statsmodels.api as sm
 
-Stock_Market = {
-    'Year': [2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2017, 2016, 2016, 2016, 2016, 2016, 2016,
-             2016, 2016, 2016, 2016, 2016, 2016],
-    'Month': [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-    'Interest_Rate': [2.75, 2.5, 2.5, 2.5, 2.5, 2.5, 2.5, 2.25, 2.25, 2.25, 2, 2, 2, 1.75, 1.75, 1.75, 1.75, 1.75, 1.75,
-                      1.75, 1.75, 1.75, 1.75, 1.75],
-    'Unemployment_Rate': [5.3, 5.3, 5.3, 5.3, 5.4, 5.6, 5.5, 5.5, 5.5, 5.6, 5.7, 5.9, 6, 5.9, 5.8, 6.1, 6.2, 6.1, 6.1,
-                          6.1, 5.9, 6.2, 6.2, 6.1],
-    'Stock_Index_Price': [1464, 1394, 1357, 1293, 1256, 1254, 1234, 1195, 1159, 1167, 1130, 1075, 1047, 965, 943, 958,
-                          971, 949, 884, 866, 876, 822, 704, 719]
-    }
+# load dataset
+df = pd.read_csv('C:\\ProgramData\\cjccl\\GitHubDesktop\\app-2.9.4\\Fall2021-AI1\\Data_Collection\\CSV_files\\AMZN\\AMZN_CombinedFiles.csv')
 
-df = pd.DataFrame(Stock_Market, columns=['Year', 'Month', 'Interest_Rate', 'Unemployment_Rate', 'Stock_Index_Price'])
+#print(df.to_string())
 
-X = df[['Interest_Rate',
-        'Unemployment_Rate']]  # here we have 2 variables for multiple regression. If you just want to use one variable for simple linear regression, then use X = df['Interest_Rate'] for example.Alternatively, you may add additional variables within the brackets
-Y = df['Stock_Index_Price']
+#df = pd.DataFrame(dataframe, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume', 'ticker', 'PriceReturn'])
 
-# with sklearn
-regr = linear_model.LinearRegression()
-regr.fit(X, Y)
+X = df[['AdjRet22Day']]
+Y = df['PriceReturn_22days']
 
-print('Intercept: \n', regr.intercept_)
-print('Coefficients: \n', regr.coef_)
+# splitting X and y into training and testing sets
+from sklearn.model_selection import train_test_split
 
-# prediction with sklearn
-New_Interest_Rate = 2.75
-New_Unemployment_Rate = 5.3
-print('Predicted Stock Index Price: \n', regr.predict([[New_Interest_Rate, New_Unemployment_Rate]]))
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size= 0.1,
+                                                    random_state=1)
 
-# with statsmodels
+# create linear regression object
+reg = linear_model.LinearRegression()
+
+# train the model using the training sets
+reg.fit(X_train, Y_train)
+
+# regression coefficients
+print('Coefficients: ', reg.coef_)
+
+# variance score: 1 means perfect prediction
+print('Variance score: {}'.format(reg.score(X_test, Y_test)))
+
+# plot for residual error
+
+## setting plot style
+plt.style.use('fivethirtyeight')
+
+## plotting residual errors in training data
+plt.scatter(reg.predict(X_train), reg.predict(X_train) - Y_train,
+            color="green", s=1, label='Train data')
+
+## plotting residual errors in test data
+plt.scatter(reg.predict(X_test), reg.predict(X_test) - Y_test,
+            color="blue", s=1, label='Test data')
+
+## plotting line for zero residual error
+plt.hlines(y=0, xmin=0, xmax=2, linewidth=2)
+
+## plotting legend
+plt.legend(loc='upper right')
+
+## plot title
+plt.title("Residual errors")
+
+## method call for showing the plot
+#plt.show()
+
+
+print('Train Score: ', reg.score(X_train, Y_train))
+print('Test Score: ', reg.score(X_test, Y_test))
+
+print('Intercept: \n', reg.intercept_)
+print('Coefficients: \n', reg.coef_)
+
 X = sm.add_constant(X)  # adding a constant
 
 model = sm.OLS(Y, X).fit()
